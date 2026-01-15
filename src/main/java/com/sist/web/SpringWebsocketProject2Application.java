0e15cc1eae236c3746b3befe11ad006879b44de4
dasolson <you@example.com>
2026-01-15 17:29:39 +0900

    20260115

---
+++ src/main/java/com/sist/web/SpringWebsocketProject2Application.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketProject2Application.class, args);
+	}
+
+}
